d7b8d76bed584d6ed199a6b3bc291d9d98095b05
Iuri Moreira <iuri@tropenhaus.com.br>
2017-10-03 19:26:46 -0300

    Added the BMI scenarios
    Added the BMI scenarios

--- app/src/main/java/moreira/iuri/imc/MainActivity.java
+++ app/src/main/java/moreira/iuri/imc/MainActivity.java
@@ -30,16 +30,45 @@ public class MainActivity extends AppCompatActivity {
             EditText txtPeso = (EditText) findViewById(R.id.txt_peso);
             EditText txtAltura = (EditText) findViewById(R.id.txt_altura);
             TextView txtResultado = (TextView) findViewById(R.id.txt_resultado);
-
+            TextView txtClassificacao = (TextView) findViewById(R.id.txt_classificacao);
 
             @Override
             public void onClick(View arg0) {
                 double peso = Double.parseDouble(txtPeso.getText().toString());
-                double altura = Double.parseDouble(txtAltura.getText().toString());
+               double altura = Double.parseDouble(txtAltura.getText().toString());
 
                 double resultado = peso/(altura*altura);
 
+                if(resultado<16){
+                    txtClassificacao.setText("Magreza Grave");
+                }
+                if(resultado>=16 && resultado<17){
+                    txtClassificacao.setText("Magreza Moderada");
+                }
+                if(resultado>=17 && resultado<18.5){
+                    txtClassificacao.setText("Magreza Leve");
+                }
+                if(resultado>=18.5 && resultado<25){
+                    txtClassificacao.setText("Saudável");
+                }
+                if(resultado>=25 && resultado<30){
+                    txtClassificacao.setText("Sobrepeso");
+                }
+                if(resultado>=30 && resultado<35){
+                    txtClassificacao.setText("Obesidade Grau I");
+                }
+                if(resultado>=35 && resultado<40){
+                    txtClassificacao.setText("Obesidade Grau II(Severa)");
+                }
+                if(resultado>40){
+                    txtClassificacao.setText("Obesidade Grau III(Mórbida)");
+                }
+
                 txtResultado.setText(String.valueOf(resultado));
+
+
+
+
 
 
 
